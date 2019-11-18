1fbfd8537c78c1558d9cc7530d9c7cbb311c7029
AspirantPeng <464959010@qq.com>
2019-11-18 16:30:33 +0800

    第三次提交

--- src/main/java/com/smart/web/UserController.java
+++ src/main/java/com/smart/web/UserController.java
@@ -49,7 +49,7 @@ public class UserController {
     }
     @RequestMapping("/test")
     public Map<String, Object>  test(@RequestParam("testParam") String test){
-        System.out.println("test git");
+        System.out.println("test");
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("msg","连接成功");
         return map;
